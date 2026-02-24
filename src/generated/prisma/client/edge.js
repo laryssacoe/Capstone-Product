d164c795cdca92c9fefc7b6a134a7ff22f14305b
laryssacoe <laryssa@uni.minerva.edu>
2026-02-24 12:11:33 +0530

    chore: add configs for testing

--- src/generated/prisma/client/edge.js
+++ src/generated/prisma/client/edge.js
@@ -557,7 +557,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- src/generated/prisma/client/index.js
+++ src/generated/prisma/client/index.js
@@ -558,7 +558,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
